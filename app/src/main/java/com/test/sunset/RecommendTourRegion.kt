038c686dd1db67abb376b7025cdf288295b2e15c
kkh <kkhyungyung0@naver.com>
2024-03-20 00:57:04 +0900

    커스텀 다이얼로그

--- app/src/main/java/com/test/sunset/RecommendTourRegion.kt
+++ app/src/main/java/com/test/sunset/RecommendTourRegion.kt
@@ -11,6 +11,9 @@ import com.test.sunset.itemss.DestinationInfo
 import com.test.sunset.itemss.NearByTourInfo
 import com.test.sunset.itemss.TourData
 
+/**
+ * gps를 통한 현재위치와 그 위치기반 근처의 관광명소 보여주는 페이지
+ */
 class RecommendTourRegion : AppCompatActivity() {
     private lateinit var binding: ActivityRecommendTourRegionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +33,7 @@ class RecommendTourRegion : AppCompatActivity() {
 
                 if (record.소재지도로명주소.contains("강원도")){ // 실제로는 현재위치를 통해 추적할것
                     Log.d("관광지명", record.관광지명)
-                    val tourelement = NearByTourInfo(record.관광지명,record.소재지지번주소)
+                    val tourelement = NearByTourInfo(record.관광지명,record.소재지지번주소,record.관리기관전화번호,record.관광지소개)
                     tourInfoList.add(tourelement)
                 }
                 // 필요한 정보들을 원하는 방식으로 처리합니다.
@@ -41,7 +44,7 @@ class RecommendTourRegion : AppCompatActivity() {
         }
         val rv_nearbytour = binding.recyclerView
         rv_nearbytour.layoutManager = LinearLayoutManager(this)
-        rv_nearbytour.adapter = NearByTourAdapter(tourInfoList)
+        rv_nearbytour.adapter = NearByTourAdapter(tourInfoList,this)
     }
     //gson 라이브러리를 사용해서 json문자열을 TourData객체로 변환하는 함수.
     private fun parseJsonToTourData(jsonString: String): TourData? {
--- app/src/main/java/com/test/sunset/adapter2/NearByTourHolder.kt
+++ app/src/main/java/com/test/sunset/adapter2/NearByTourHolder.kt
@@ -1,17 +1,33 @@
 package com.test.sunset.adapter2
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.test.sunset.R
 import com.test.sunset.databinding.TourRegionItemBinding
 import com.test.sunset.itemss.NearByTourInfo
 
-class NearByTourHolder(private val binding : TourRegionItemBinding) : RecyclerView.ViewHolder(binding.root) {
-
+class NearByTourHolder(
+    private val binding: TourRegionItemBinding,
+    private val context: Context
+) : RecyclerView.ViewHolder(binding.root) {
     fun bind(nearByTourInfo: NearByTourInfo) {
-        binding.nearByTourDataInfo =
-            nearByTourInfo //데이터와 레이아웃을 연결시켜준다.
-            //recyclerview의 데이터가 하나씩 쌓일때마다 생성. nearbytourdatainfo와 연결
+        binding.nearByTourDataInfo = nearByTourInfo
 
+        itemView.setOnClickListener {
 
+            val view = LayoutInflater.from(context).inflate(R.layout.custom_dialog_item, null)
+            view.findViewById<TextView>(R.id.tv_region).text = nearByTourInfo.관광지명
+            view.findViewById<TextView>(R.id.tv_address).text = nearByTourInfo.소재지지번주소
+            view.findViewById<TextView>(R.id.tv_call).text = nearByTourInfo.관리기관전화번호
+            view.findViewById<TextView>(R.id.tv_info).text = nearByTourInfo.관광지소개
+
+            val alertDialogBuilder = AlertDialog.Builder(context,R.style.CustomAlertDialog)
+                .setView(view)
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
     }
 }
-
